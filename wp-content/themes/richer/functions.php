c4b65d72201470cd1d49eb57b7db9f2e3e9efd5c
victorli <luckylzs@gmail.com>
2016-09-11 11:17:05 +0800

    solve avatar error by using https to replace http

--- wp-content/themes/richer/functions.php
+++ wp-content/themes/richer/functions.php
@@ -682,6 +682,14 @@ if(!function_exists('asw_wp_title')) {
 	}
 	add_filter('wp_title', 'asw_wp_title', 10, 2);
 }
+/**
+ * Using https to replace http protocal to solve the error when getting avatar
+ */
+function get_ssl_avatar($avatar) {
+	$avatar = preg_replace('/.*\/avatar\/(.*)\?s=([\d]+)&.*/','<img src="https://secure.gravatar.com/avatar/$1?s=$2" class="avatar avatar-$2" height="$2" width="$2">',$avatar);
+	return $avatar;
+}
+add_filter('get_avatar', 'get_ssl_avatar');
 /* ------------------------------------------------------------------------ */
 /* EOF
 /* ------------------------------------------------------------------------ */
